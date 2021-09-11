e26dda11a7af08ca1d0d6fb2fc2897fae9e52075
James Cole <jamescole.uk@gmail.com>
2021-09-11 10:25:27 +0100

    Improve display

--- src/main.rs
+++ src/main.rs
@@ -144,13 +144,23 @@ blow_mind() {
     } else {
         let example_command = format!("./{} {}", script, functions[0].name);
         print!("{}", script.on_blue());
-        println!(" - Usage: {}\n", example_command.green());
+        println!(" - Usage: {}\n", example_command.blue());
 
+        // Get the longest function name
+        const INDENT: usize = 2;
+        let padding = functions
+            .iter()
+            .max_by(|x, y| x.name.len().cmp(&y.name.len()))
+            .unwrap()
+            .name
+            .len()
+            + INDENT;
         for function in functions {
+            // We'll pad right so everything aligns nicely.
             // First print the function name
             let to_print = function
                 .name
-                .pad_to_width_with_alignment(20, Alignment::Right)
+                .pad_to_width_with_alignment(padding, Alignment::Right)
                 .green();
             if function.comment.len() > 0 {
                 print!("{}", to_print);
@@ -163,7 +173,11 @@ blow_mind() {
                 if i == 0 {
                     println!(" {}", line);
                 } else {
-                    println!(" {:20}{}", "", line);
+                    println!(
+                        "{} {}",
+                        "".pad_to_width_with_alignment(padding, Alignment::Right),
+                        line
+                    );
                 }
             });
         }
--- test/long_script.sh
+++ test/long_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This function is very clever and awesome and does a lot of neat stuff.
-# And here is some more detailed description about this funciton. Isn't it great?
+# And here is some more detailed. wefsdjfklsdjhf
 some_function() {
     echo "hello world from a script"
     echo "foobar"
@@ -15,6 +15,11 @@ another_function() {
 
 # More functions
 yet_more_functions() {
+    echo "hello from another function"
+}
+
+# This function has a stupidly long name and it won't render that well.
+really_really_really_really_really_really_long_function_name() {
     echo "hello from another function"
 }
 
