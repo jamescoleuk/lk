cec9d42a9ace02c547225b6c5ac607744ab4b4d9
James Cole <jamescole.uk@gmail.com>
2021-09-22 05:50:26 +0100

    Tidy up

--- tests/long_script.sh
+++ tests/long_script.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-# This function is very clever and awesome and does a lot of neat stuff.
-# And here is some more detailed. wefsdjfklsdjhf
-some_function() {
-    echo "hello world from a script"
-    echo "foobar"
-    sleep 1
-    echo "ending function now"
-}
-
-another_function() {
-    echo "hello from another function"
-}
-
 # More functions
 yet_more_functions() {
     echo "hello from another function"
