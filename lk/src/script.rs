67c79d62df920034940d3b90220d1801871113be
David Cole <david@arwen.ai>
2022-08-02 14:47:41 +0100

    Made variable unmutable

--- lk/src/script.rs
+++ lk/src/script.rs
@@ -92,7 +92,7 @@ impl Script {
     }
 
     pub fn path(&self) -> String {
-        let mut path = self.path.clone();
+        let path = self.path.clone();
         return path.as_os_str().to_string_lossy().to_string();
     }
 
